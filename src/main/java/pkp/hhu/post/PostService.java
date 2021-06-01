f983ca976cc80f049fa9d33c9dabe371c1f33795
Kate <reczniczka@o2.pl>
2021-06-01 15:18:58 +0200

    Formularz do dodawania miejsc i postów do bazy danych

--- src/main/java/pkp/hhu/post/PostService.java
+++ src/main/java/pkp/hhu/post/PostService.java
@@ -1,9 +1,16 @@
 package pkp.hhu.post;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class PostService {
     PostRepository postRepository;
 
     public PostService(PostRepository postRepository) {
         this.postRepository = postRepository;
+    }
+
+    public void save(Post post) {
+        postRepository.save(post);
     }
 }
